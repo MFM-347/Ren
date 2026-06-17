be91c02a4bf21e5c381d51494de528ed0a7411db
MFM-347 <madnifm347@outlook.com>
2026-06-17 12:17:17 +0500

    update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@ import com.diffplug.spotless.kotlin.KtfmtStep.TrailingCommaManagementStrategy
 
 plugins {
   alias(libs.plugins.android.application) apply false
-  alias(libs.plugins.kotlin.android) apply false
   alias(libs.plugins.kotlin.compose) apply false
   alias(libs.plugins.spotless) apply true
 }
